e70c3d5b5fcaece9b88e06cdc85488ace7670dcb
MauseMarc <gomme@gmx.ch>
2024-09-26 10:44:31 +0200

    yurr

---
+++ TimeCalcModule.py
@@ -0,0 +1,25 @@
+def validateTime(time):
+    valTime=[]
+    if ":" in time or "," in time or "." in time:
+        time=time.replace(" ",":").replace(",",":").replace(".",":")
+        valTime=time.split(":")
+    elif len(time) == 3 or len(time) == 4:
+            valTime.append(time[:-2])
+            valTime.append(time[-2:])
+    else:
+        return False
+
+    if valTime[0].isdigit() and valTime[1].isdigit():
+        valTime[0]=int(valTime[0])
+        valTime[1]=int(valTime[1])
+    else:
+        return False
+    if valTime[0] >23:
+        return False
+    if valTime[1] >59:
+        return False
+    minuteTime=valTime[0]*60+valTime[1]
+    return valTime,minuteTime
+
+
+print(validateTime("1758"))
