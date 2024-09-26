ccae3a6c910c7c8174440bb47a775ff57af35e13
HomieDerPrakti <julian.elias.egger@outlook.com>
2024-09-26 14:32:09 +0000

    ui textfelder

--- main.py
+++ main.py
@@ -7,8 +7,23 @@ class App:
         self.root = root
         root.title("Arbeitszeitrechner")
 
-        self.inputtxt = tk.Text(height= 1, width= 10)
-        self.inputtxt.pack(pady=10)
+        self.starttime_title = tk.Label(root, text="Startzeit eingeben")
+        self.starttime_title.pack(pady=10)
+
+        self.starttime = tk.Text(height= 1, width= 10)
+        self.starttime.pack(pady=10)
+
+        self.mittag_start_title = tk.Label(root, text="Mittag Start eingeben")
+        self.mittag_start_title.pack(pady=10)
+
+        self.mittag_start = tk.Text(height= 1, width= 10)
+        self.mittag_start.pack(pady=10)
+
+        self.mittag_end_title = tk.Label(root, text="Mittag Ende eingeben")
+        self.mittag_end_title.pack(pady=10)
+
+        self.mittag_end = tk.Text(width=10, height=1)
+        self.mittag_end.pack(pady=10)
 
         self.button = tk.Button(root, text="print", command=self.printt)
         self.button.pack(pady=10)
@@ -17,7 +32,7 @@ class App:
         self.lbl.pack(pady=10)
     
     def printt(self):
-        input_text = self.inputtxt.get(1.0, "end-1c")
+        input_text = self.starttime.get(1.0, "end-1c")
         self.lbl.config(text=input_text)
 
 if __name__ == "__main__":
