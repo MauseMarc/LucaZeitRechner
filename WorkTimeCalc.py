f680af0dd97c2db8c57fbb33442ab38369b1d50e
MauseMarc <gomme@gmx.ch>
2024-09-27 13:42:56 +0200

    godspeed babyyy

--- WorkTimeCalc.py
+++ WorkTimeCalc.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def minToHour(minutes):
     calc = minutes % 60
     minut = calc
@@ -44,3 +45,53 @@ def minToHour(minutes):
 print("I commited changes to the federal law of switzerland")
 print("Hello world")
 print("this should be a branceh")
+=======
+from TimeCalcModule import *
+import time
+import datetime
+import tkinter as tk
+from tkinter import messagebox
+
+def minMax(morning, mid, midend):
+        check = [morning, mid, midend]
+        for test in check:
+            if not validateTime(test):
+                return False
+        morningTime = validateTime(mid) - validateTime(morning)
+        workTimes=[492,540]
+        dayFinish=[]
+        for minmax in workTimes:
+            workHours = minmax - morningTime
+            workHours += validateTime(midend)
+            workHours = convertMinutes(workHours)
+            dayFinish.append(workHours)
+        return dayFinish
+
+def show_popup():
+    root=tk.Tk()
+    root.lift()
+    root.attributes('-topmost', True)
+    root.withdraw()
+    messagebox.showinfo("Popup", "Arbeitszeit minimum erreicht!")
+    root.destroy()
+
+def wait(minMaxEnd):
+    now = datetime.datetime.now()
+    conversion=str(now.time())
+    currentTime=conversion.split(":")
+    seconds= currentTime[-1].split(".")
+    seconds=int(seconds[0])
+    currentTime=currentTime[:-1]
+
+    nowTime=int(currentTime[0])*60+int(currentTime[1])
+    endTime=minMaxEnd[0]*60+minMaxEnd[1]
+
+    waitTime=(endTime-nowTime)*60-seconds
+    print("alarm will appear in",int((waitTime-waitTime%60)/60),"minutes and",waitTime%60,"seconds")
+
+    if waitTime < 1:
+        return False
+    else:
+        time.sleep(waitTime)
+        show_popup()
+>>>>>>> Stashed changes
---
+++ main.py
@@ -0,0 +1,61 @@
+from WorkTimeCalc import *
+import tkinter as tk
+from tkinter import messagebox
+
+class App:
+    def __init__(self, root):
+
+        self.root = root
+        root.title("Arbeitszeitrechner")
+
+        self.starttime_title = tk.Label(root, text="Startzeit eingeben")
+        self.starttime_title.pack(pady=10)
+
+        self.starttime = tk.Text(height= 1, width= 10)
+        self.starttime.pack(pady=10)
+
+        self.mittag_start_title = tk.Label(root, text="Mittag Start eingeben")
+        self.mittag_start_title.pack(pady=10)
+
+        self.mittag_start = tk.Text(height= 1, width= 10)
+        self.mittag_start.pack(pady=10)
+
+        self.mittag_end_title = tk.Label(root, text="Mittag Ende eingeben")
+        self.mittag_end_title.pack(pady=10)
+
+        self.mittag_end = tk.Text(width=10, height=1)
+        self.mittag_end.pack(pady=10)
+
+        self.button = tk.Button(root, text="print", command=self.calc_time)
+        self.button.pack(pady=10)
+
+        self.lbl1 = tk.Label(text="")
+        self.lbl1.pack(pady=10)
+
+        self.lbl2 = tk.Label(text="")
+        self.lbl2.pack(pady=10)
+
+    def calc_time(self):
+        start_time = self.starttime.get(1.0, "end-1c")
+        mittag_start = self.mittag_start.get(1.0, "end-1c")
+        mittag_end = self.mittag_end.get(1.0, "end-1c")
+        min_max_time = minMax(start_time, mittag_start, mittag_end)
+        if not min_max_time:
+            messagebox.showinfo(title="Fehler", message="Zeiteingabe ungültig")
+        else:
+            min_time_t = min_max_time[0]
+            max_time_t = min_max_time[1]
+            min_time = f"{min_time_t[0]:02}:{min_time_t[1]:02}"
+            max_time = f"{max_time_t[0]:02}:{max_time_t[1]:02}"
+
+            self.lbl1.configure(text=f"Minimale Zeit: {min_time}")
+            self.lbl2.configure(text=f"Maximale Zeit: {max_time}")
+
+
+
+
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     app = App(root)
+#     root.geometry("300x400")
+#     root.mainloop()
