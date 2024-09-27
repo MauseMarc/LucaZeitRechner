82de118b14249bf3e3e945c52274b21e73b155ec
HomieDerPrakti <julian.elias.egger@outlook.com>
2024-09-27 16:00:46 +0200

    popup not yet working but kinda

--- WorkTimeCalc.py
+++ WorkTimeCalc.py
@@ -1,4 +1,5 @@
 from TimeCalcModule import *
+import datetime
 
 def minMax(morning, mid, midend):
         check = [morning, mid, midend]
@@ -14,3 +15,16 @@ def minMax(morning, mid, midend):
             workHours = convertMinutes(workHours)
             dayFinish.append(workHours)
         return dayFinish
+
+def time_until_end(end):
+    now = datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")
+    seconds = int(datetime.datetime.strftime(datetime.datetime.now(), "%S"))
+    now_min = validateTime(now)
+    end_min = validateTime(end)
+    timediff_min = end_min - now_min
+    waitTime= timediff_min*60-seconds
+    leftoverSeconds= waitTime%60
+    minutes=int((waitTime-leftoverSeconds)/60)
+    yeeees= convertMinutes(minutes)
+    totalt_time= f"{yeeees[0]:02}:{yeeees[1]:02}:{leftoverSeconds:02}"
+    return totalt_time, waitTime
--- main.py
+++ main.py
@@ -1,82 +1,59 @@
 import tkinter as tk
 from tkinter import messagebox
-
-def minMax(morning, mid, midend):
-        check = [morning, mid, midend]
-        for test in check:
-            if not validateTime(test):
-                return False
-        morningTime = validateTime(mid) - validateTime(morning)
-        workTimes=[492,540]
-        dayFinish=[]
-        for minmax in workTimes:
-            workHours = minmax - morningTime
-            workHours += validateTime(midend)
-            workHours = convertMinutes(workHours)
-            dayFinish.append(workHours)
-        return dayFinish
-
-def validateTime(time):
-    valTime=[]
-    if ":" in time or "," in time or "." in time:
-        time=time.replace(" ",":").replace(",",":").replace(".",":")
-        valTime=time.split(":")
-    elif len(time) == 3 or len(time) == 4:
-            valTime.append(time[:-2])
-            valTime.append(time[-2:])
-    else:
-        return False
-    if valTime[0].isdigit() and valTime[1].isdigit():
-        valTime[0]=int(valTime[0])
-        valTime[1]=int(valTime[1])
-    else:
-        return False
-    if valTime[0] >23 or valTime[1] >59:
-        return False
-    minuteTime=valTime[0]*60+valTime[1]
-    return minuteTime
-
-def convertMinutes(totalMinutes):
-    overspill = totalMinutes % 60
-    hour = (totalMinutes - overspill) / 60
-    return int(hour),overspill
-
+from WorkTimeCalc import *
+import time
+from tkinter import messagebox
 class App:
     def __init__(self, root):
         self.root = root
         root.title("Arbeitszeitrechner")
 
         self.starttime_title = tk.Label(root, text="Startzeit eingeben")
-        self.starttime_title.pack(pady=10)
+        self.starttime_title.pack(pady=1)
 
-        self.starttime = tk.Text(height= 1, width= 10)
-        self.starttime.pack(pady=10)
+        self.starttime = tk.Entry()
+        self.starttime.pack(pady=1)
 
         self.mittag_start_title = tk.Label(root, text="Mittag Start eingeben")
-        self.mittag_start_title.pack(pady=10)
+        self.mittag_start_title.pack(pady=1)
 
-        self.mittag_start = tk.Text(height= 1, width= 10)
-        self.mittag_start.pack(pady=10)
+        self.mittag_start = tk.Entry()
+        self.mittag_start.pack(pady=1)
 
         self.mittag_end_title = tk.Label(root, text="Mittag Ende eingeben")
-        self.mittag_end_title.pack(pady=10)
+        self.mittag_end_title.pack(pady=1)
 
-        self.mittag_end = tk.Text(width=10, height=1)
-        self.mittag_end.pack(pady=10)
+        self.mittag_end = tk.Entry()
+        self.mittag_end.pack(pady=1)
 
-        self.button = tk.Button(root, text="print", command=self.calc_time)
+        self.button = tk.Button(root, text="print", command=lambda:[self.calc_time()])
         self.button.pack(pady=10)
+        
+        self.notification = tk.IntVar()
+        self.notification_switch = tk.Checkbutton(root, text="Benachrichtigung beim Erreichen der Zeit", variable=self.notification, onvalue=1, offvalue=0, command= self.update_notification_status)
+        self.notification_switch.pack(pady=5)
 
         self.lbl1 = tk.Label(text="")
-        self.lbl1.pack(pady=10)
+        self.lbl1.pack(pady=5)
 
         self.lbl2 = tk.Label(text="")
-        self.lbl2.pack(pady=10)
+        self.lbl2.pack(pady=5)
+
+        self.lbl3 = tk.Label(text="Benachrichtigung aus")
+        self.lbl3.pack(pady=5)
+
+        self.lbl4 =tk.Label(text="")
+        self.lbl4.pack(pady=5)
+
+        self.min_time = 0
+        self.max_time = 0
+        self.wait_time = 0
+        self.popup_v = 0
 
     def calc_time(self):
-        start_time = self.starttime.get(1.0, "end-1c")
-        mittag_start = self.mittag_start.get(1.0, "end-1c")
-        mittag_end = self.mittag_end.get(1.0, "end-1c")
+        start_time = self.starttime.get()
+        mittag_start = self.mittag_start.get()
+        mittag_end = self.mittag_end.get()
         min_max_time = minMax(start_time, mittag_start, mittag_end)
         if not min_max_time:
             messagebox.showinfo(title="Fehler", message="Zeiteingabe ungültig")
@@ -86,13 +63,43 @@ class App:
             min_time = f"{min_time_t[0]:02}:{min_time_t[1]:02}"
             max_time = f"{max_time_t[0]:02}:{max_time_t[1]:02}"
 
-            self.lbl1.configure(text=f"Minimale Zeit: {min_time}")
-            self.lbl2.configure(text=f"Maximale Zeit: {max_time}")
+            self.lbl1.configure(text=f"Minimale Zeit: {min_time}", bg="#B0BEC5")
+            self.lbl2.configure(text=f"Maximale Zeit: {max_time}", bg="#B0BEC5")
+            self.min_time = min_time
+            self.max_time = max_time
+            self.display_time_until_end()
+            self.popup_v = 1
+
+    def update_notification_status(self):
+        if self.notification.get() == 0:
+            self.lbl3.config(text="Benachrichtigung aus")
+            return
+        else:
+            self.lbl3.config(text="Benachrichtigung ein")
+            return
+    
+    def display_time_until_end(self):
+        until_end, wait_time = time_until_end(self.min_time)
+        self.lbl4.config(text=until_end)
+        self.wait_time = wait_time
+    
+    def popup(self):
+        wait_time = self.wait_time
+        if wait_time < 1:
+            return
+        else:
+            print("slep")
+            time.sleep(wait_time)
+            if self.notification == 1:    
+                messagebox.showinfo("8:12 erreicht!")
+    
+
+
 
 def main():
     root = tk.Tk()
     app = App(root)
-    root.geometry("300x400")
+    root.geometry("300x500")
     root.mainloop()
 
 if __name__ == "__main__":
